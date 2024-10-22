9dda0f498d94903506949d89d7693dfbdbe64c6a
megrose10 <157438545+megrose10@users.noreply.github.com>
2024-10-22 09:59:24 -0600

    Update code.js

--- code.js
+++ code.js
@@ -12,6 +12,15 @@ function flip(array, n) {
 
 // Use only flip() here to manipulate the array
 function pancakeSort(array) {
+
+    let size = array.length();
+    for(let i=0;i<size;i++) {
+        for(let j=0;i<size-1;j++) {
+            if(array[i] > array[j]) {
+                array = flip(array, j);
+            }
+    }
+    
     return array;
 }
 

874d5238090dbfcb2480c2ee0cb9e085efea501a
megrose10 <157438545+megrose10@users.noreply.github.com>
2024-10-22 10:25:57 -0600

    Update code.js

--- code.js
+++ code.js
@@ -14,13 +14,16 @@ function flip(array, n) {
 function pancakeSort(array) {
 
     let size = array.length();
+    if(size == 0){
+        return 0;
+    }
     for(let i=0;i<size;i++) {
         for(let j=0;i<size-1;j++) {
             if(array[i] < array[j]) {
                 flip(array, size);
             }
             if(size<0){
-            size = size - 1;
+                size = size - 1;
             }
             else return array;
         }

efc0753d616528d5ff07bd652352c61fca48c0e4
megrose10 <157438545+megrose10@users.noreply.github.com>
2024-11-23 14:56:05 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,14 +1,14 @@
 function flip(array, n) {
     //console.log(array[0]);
     //console.log(array[2]);
-    for(let i = 0; i < (n - 1); i++) {
-      let tmp = array[i];
+    for(let first = 0; first < (n - 1); first++) {
+      let tmp = array[first];
       //console.log(tmp + "tmp1");
-      array[i] = array[i+1];
+      array[first] = array[first+1];
       //console.log(array[first++]);
       //console.log(array[0]);
       //console.log(tmp + "tmp2");
-      array[i+1] = tmp;
+      array[first+1] = tmp;
       //console.log(tmp);
       //console.log(array[first++] + "test");
     }
@@ -19,25 +19,23 @@ function flip(array, n) {
 //console.log(flip(array, 2));
 //Use only flip() here to manipulate the array
 function pancakeSort(array) {
-  let size = array.length;
   let max = 0;
-  while(size > 1) {
-    for(let i = 1; i < size; i++) {
-      if(array[i] > array[max]) {
-        max = i;
+  for(let i = array.length; i > 1; i--) {
+    for(let j = 1; j < i; j++) {
+      if(array[j] > array[max]) {
+        max = j;
       }
     }
     if(max == 0) {
-      array = flip(array, size);
+      array = flip(array, i);
     }
     else {
       array = flip(array, max + 1);
-      array = flip(array, size);
+      array = flip(array, i);
     }
-    size--;
   }
   return array;
 }
 //let aTest = [5, 4, 3, 1, 2];
 //let newATest = pancakeSort(aTest);
-//console.log(pancakeSort(aTest));
+//console.log(pancakeSort(aTest)); 
